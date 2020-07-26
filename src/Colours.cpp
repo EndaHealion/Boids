1a177b25e791d075d2938fe0db32460c6d447cd8
kroydlowen <58573017+kroydlowen@users.noreply.github.com>
2020-07-26 12:06:29 +0100

    Update Colours.cpp
    Added a check to see if param angle is within the ranges of the atan2 function. There was a posibility that atan2 could return NaN, which would give an array subscript out of range exception.

--- src/Colours.cpp
+++ src/Colours.cpp
@@ -2,7 +2,11 @@
 
 // takes in an angle and return an associated colour
 sf::Color Colours::chooseColour(float angle) {
-	// probably a faster was of doing this...
-	float normalisedAngle = (angle + 3.141592635f) / (2 * 3.141592635f);
-	return colourArray[(int)(normalisedAngle * (colourArray.size()-1))];
+	const float twoPie = 2 * 3.141592635f;
+	if (angle >= -twoPie && angle <= twoPie) {
+		float normalisedAngle = (angle + 3.141592635f) / twoPie;
+		return colourArray[(int)(normalisedAngle * (colourArray.size() - 1))];
+	}
+	else return sf::Color::White;
 }
+
